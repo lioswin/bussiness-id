2a6a9fdc1702ead4d510944130a28ca5435e4b0a
lioswin <lioswinrite@gmail.com>
2022-11-28 19:24:33 +0300

    chnages to some css

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
 <div className='app'>
   <Info/>
   <About/>
-  <Interest/>
   <Footer/>
 </div>
   );
--- src/Info.js
+++ src/Info.js
@@ -1,22 +1,27 @@
 import React from "react"
 import myImage from './resources/ega.jpeg'
+import Interest from "./Interest";
 function Info(){
     return (
         <div>
-        <img src={myImage} alt="profile" width="500px" height="500px"className="image"/>
+        <img src={myImage} alt="profile" className="image"/>
+        <div className="grey">
         <div className="container-name">
         <h2>Tony Rite</h2>
         <h3>Frontend developer</h3>
         <h4>lioswin.website</h4>
         </div> 
-        <div className="btn">
-            <button className="">
+        <div className="">
+            <button className="btn2">
             <i className="fa fa-envelope"></i>Email</button>
-            <button className="">
-            <i class='fa fa-linkedin'></i>LinkedIn</button>
+            <button className="btn">
+            <i className="fa fa-linkedin"></i>LinkedIn</button>
+        </div>
+        <Interest/>
         </div>
         
         </div>
+
     )
 }
 
