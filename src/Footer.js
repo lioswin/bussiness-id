1928d46bab4f41a9eb2695caec361cefcd3fc120
lioswin <lioswinrite@gmail.com>
2022-11-29 01:25:35 +0300

    finished css styling

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,14 @@ import React from "react"
 
 function Footer(){
     return (
+        <div class="footer">
        <ul>
-        <li></li>
+        <li><i className="fa fa-twitter-square"></i></li>
+        <li><i className="fa fa-facebook-square"></i></li>
+        <li><i className="fa fa-instagram"></i></li>
+        <li><i className="fa fa-github"></i></li>
        </ul>
+       </div>
        
     )
 }
