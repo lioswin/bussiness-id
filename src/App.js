eeb1c6f20564851c482db8334884eb26c1c61721
lioswin <lioswinrite@gmail.com>
2022-11-28 13:30:32 +0300

    initialised the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './About.js'
+import Info from './info.js'
+import Footer from './Footer.js'
+import Interest from './Interest' 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <Info/>
+  <About/>
+  <Interest/>
+  <Footer/>
+</div>
   );
 }
 
---
+++ src/Info.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Info(){
+    return (
+        <div>
+        <img src="" alt="profile" className="image"/>
+        <h2>Tony Rite</h2>
+        <h3>Frontend developer</h3>
+        <h5>lioswin.website</h5>
+        <div>
+        <button>
+        <i className="fa fa-envelope"></i>Email</button>
+        <button>
+        <i class='fa fa-linkedin'></i>LinkedIn</button>
+        </div>
+        </div>
+    )
+}
+
+export default Info;
